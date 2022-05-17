70ab00fbec730f73e112b19dcef2e68cc065d8db
JUAN <juanoramirez1503@gmail.com>
2022-05-17 17:38:55 -0500

    my first repository

---
+++ calculadora.js
@@ -0,0 +1,2 @@
+< table width = "15%"
+border = "1" > < /table>
---
+++ practica.js
@@ -0,0 +1,12 @@
+function saludo() {
+    alert("Bienvenidos al sena");
+
+}
+saludo();
+
+
+
+var miboton = document.getElementById("boton");
+miboton.style.width = "50px";
+miboton.style.height = "50px";
+miboton.style.backgroundColor = "red";
